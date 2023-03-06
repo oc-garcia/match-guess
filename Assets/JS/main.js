ca28d3a8f4ff575cc0ec27970d6e6d2bb0a0a163
oc-garcia <octaviogarcia1988@gmail.com>
2023-03-06 10:19:20 -0300

    Match Guesser project

---
+++ Assets/JS/main.js
@@ -0,0 +1,24 @@
+let nrGuesses = document.getElementById("nrGuesses");
+let resultBtn = document.getElementById("resultBtn");
+let resultField = document.getElementById("resultField");
+let resultContainer = document.getElementById("resultContainer");
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+let results = [];
+
+resultBtn.addEventListener("click", () => {
+  event.preventDefault();
+  results = [];
+  for (let i = 0; i < nrGuesses.value; i++) {
+    let result = `<li>Result nr. ${i + 1} : ${getRandomInt(5)} x ${getRandomInt(5)}</li><br>`;
+    results.push(result);
+    console.log(results);
+    resultContainer.className = "resultContainerOn";
+  }
+  results.forEach((result) => {
+    resultField.innerHTML += result;
+  });
+});
